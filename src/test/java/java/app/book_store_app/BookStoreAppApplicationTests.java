4b2008ba4e21915044471129aa501df8d3052a46
Mateusz <mateusz.kiryk@o2.pl>
2025-05-15 11:39:30 +0200

    Initial structure

---
+++ src/test/java/java/app/book_store_app/BookStoreAppApplicationTests.java
@@ -0,0 +1,13 @@
+package java.app.book_store_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

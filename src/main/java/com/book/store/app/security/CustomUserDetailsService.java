66022b2f4aeb65bfcece83ad147f23e00854b45b
Mateusz <mateusz.kiryk@o2.pl>
2025-05-26 12:53:21 +0200

    Add Spring Security with role-based access and Liquibase updates

---
+++ src/main/java/com/book/store/app/security/CustomUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.book.store.app.security;
+
+import com.book.store.app.entity.User;
+import com.book.store.app.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: "
+                        + email));
+        return new CustomUserDetails(user);
+    }
+}
